3564258bcfa3696a45891f2cb3fd6ce6ac322791
miguellouzada <140673704+miguellouzada@users.noreply.github.com>
2026-05-15 22:10:41 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,25 @@
+const CACHE_NAME = "agendamentos-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", (event) => {
-    console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("activate", (event) => {
-    console.log("Service Worker ativo");
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
